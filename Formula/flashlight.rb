29b7eb866ce78bb600ad9682713ccec04d58b333
Bram Wasti <bwasti@fb.com>
2022-08-23 19:04:45 -0400

    Update flashlight.rb

--- Formula/flashlight.rb
+++ Formula/flashlight.rb
@@ -12,7 +12,8 @@ class Flashlight < Formula
   def install
     system "cmake", "-S", ".", "-B", "build", "-DFL_ARRAYFIRE_USE_CPU=ON", \
            "-DFL_BUILD_DISTRIBUTED=OFF", "-DFL_USE_ONEDNN=OFF", "-DFL_BUILD_TESTS=OFF", \
-           "-DFL_BUILD_EXAMPLES=OFF", "-DFL_BUILD_SCRIPTS=OFF", *std_cmake_args
+           "-DFL_BUILD_EXAMPLES=OFF", "-DFL_BUILD_SCRIPTS=OFF", "-DBUILD_SHARED_LIBS=ON", \
+           *std_cmake_args
     system "make", "-C", "build", "install", "-j4"
   end
 
