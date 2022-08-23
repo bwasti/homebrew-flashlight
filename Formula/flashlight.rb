e632581a33bd64027d5c9a540efe5818598ca030
Bram Wasti <bwasti@fb.com>
2022-08-23 19:59:56 -0400

    point to diff repo

--- Formula/flashlight.rb
+++ Formula/flashlight.rb
@@ -1,9 +1,9 @@
 class Flashlight < Formula
   desc "Standalone C++ library for machine learning"
   homepage "https://fl.readthedocs.io/en/latest"
-  url "https://github.com/flashlight/flashlight/archive/de7a65b.tar.gz"
+  url "https://github.com/jacobkahn/flashlight/archive/c343e5c.tar.gz"
   version "0.3.2"
-  sha256 "3c6fa27c1be2ad622e79530633fec20ac88dd1fee55a3a5c1bbe88fcf06811f9"
+  sha256 "11f4c7dbc2013a44b073639ffab39897c34e8c76a61d459e655ea0b6ff8b6828"
   license "MIT"
 
   depends_on "arrayfire" => :build
